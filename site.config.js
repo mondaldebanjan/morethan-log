43ac534d434990ee8e302b02876c8057d97ffe52
Debanjan Mondal <mondal.debanjan.bwn@gmail.com>
2024-04-07 18:01:44 +0530

    initial chanes

--- site.config.js
+++ site.config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "debanjan-notes",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Knowlwdge Seeker",
+    bio: "I never stop learning..",
+    email: "mondal.debanjan.bwn@gmail.com",
+    linkedin: "mondaldebanjanbwn",
+    github: "mondaldebanjanbwn",
+    instagram: "__debanjan",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `debajnan-notes`,
+      href: "https://github.com/mondaldebanjan/morethan-log",
     },
   ],
   // blog setting (required)
@@ -23,8 +23,8 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://debanjan-notes.vercel.app",
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
